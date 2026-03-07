c9a23244930ab0ea744678bb57efaffc2e9a6bfb
Eclipses <99494277+EclipsesDev@users.noreply.github.com>
2026-03-07 17:00:47 +0700

    (+) added more local svg

--- projects/script.js
+++ projects/script.js
@@ -21,7 +21,7 @@ async function openProject(project) {
     const isAuthenticated = await hasVideoCloudSession();
     window.location.href = isAuthenticated
       ? "/projects/video-cloud/"
-      : "/projects/video-cloud/login.html";
+      : "/projects/video-cloud/login";
     return;
   }
 
